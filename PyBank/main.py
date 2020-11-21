1567010aceba7f17b6e889f0466a4dee85a6915e
Garrett DeBlois <garrettdeblois13@yahoo.com>
2020-11-20 20:44:15 -0600

    modify code to be able to display which months have the minimum and maximum profit/loss change

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,30 +9,36 @@ with open(filePath) as csvfile: #open the path to the CSV file as a new object
     csv_header = next(csvreader) #skipping the header so we get the correct count of rows
     print("Financial Analysis")
     print("----------------------------")
-    rowCounter, total, avgChange, maxIncrease, minIncrease = 0, 0, 0, 0, 0
+    rowCounter, total, avgChange, maxIncreaseAmount, maxIncreaseMonth, minIncreaseAmount, minIncreaseMonth  = 0, 0, 0, 0, 0, 0, 0
     monthlyValues = [] #declaring empty list to keep track of individual monthly profit/loss values from the CSV
-    monthlyChange = [] #declaring empty list to keep track of the chance in profit/loss across each month
+    monthlyChangeAmount = [] #declaring empty list to keep track of the change in profit/loss across each month
+    monthlyChangeMonth = [] #declaring empty list to keep track of the month corresponding to the amount of the change in profit/loss
 
     for row in csvreader:#Read each row of data after the header
         monthlyValues.append(row[1]) #appending value of Profit/Loss column of the CSV to the monthlyValues list
         total += int((row[1])) #calculating total amount of profit/loss over the entire period
         #passing two values to CalculateChange function. the first parameter, startingValue, will be equal to the previous value of the iterator of the loop 
         #which would be the current position in the list monthlyValues - 1. the second parameter, endingValue, will be equal to the current value of the 
-        # iterator of the loop. this value is then added to my empty list titled "monthlyChange"
-        monthlyChange.append(CalculateChange(int(monthlyValues[rowCounter-1]), int(row[1])))
+        # iterator of the loop. this value is then added to my empty list titled "monthlyChangeAmount"
+        monthlyChangeAmount.append(CalculateChange(int(monthlyValues[rowCounter-1]), int(row[1])))
+        monthlyChangeMonth.append(row[0]) #keep track of the months so I can map the name of the month to the value of the change for that month
         #keeping track of the row count for two reasons: 1. to be able to get the proper index for my list 
         #and 2. to count the number of months of data (each month has its own row in the CSV) 
         rowCounter +=1 
     #calculating average by taking the sum of the list divided by the length of the list. subtracing one from the length because the value from the first iteration 
     #of the loop will be discarded since the value is zero as there is no prior value to calculate amount change to month one. rounding result to 2 decimal places.
-    avgChange =  round(sum(monthlyChange) / (len(monthlyChange)-1),2) 
-    maxIncrease = max(monthlyChange) #maximum value from the monthlyChange list
-    minIncrease = min(monthlyChange) #minimum value from the monthlyChange list
+    avgChange =  round(sum(monthlyChangeAmount) / (len(monthlyChangeAmount)-1),2) 
+    maxIncreaseAmount = max(monthlyChangeAmount) #maximum value from the monthlyChangeAmount list
+    minIncreaseAmount = min(monthlyChangeAmount) #minimum value from the monthlyChangeAmount list
+    #getting the index of the max value from the Amounts list and using that value as the index for the Months list since they should be 
+    #at the same position since they correspond to the same row in the CSV
+    maxIncreaseMonth = monthlyChangeMonth[monthlyChangeAmount.index(max(monthlyChangeAmount))]
+    minIncreaseMonth = monthlyChangeMonth[monthlyChangeAmount.index(min(monthlyChangeAmount))]  
 
     print(f"Total Months: {rowCounter}")
     print(f"Total: ${total}")
     print(f"Average Change: ${avgChange}")
-    print(f"Greatest Increase in Profits: ${maxIncrease}")
-    print(f"Greatest Decrease in Profits: ${minIncrease}")
+    print(f"Greatest Increase in Profits: {maxIncreaseMonth} (${maxIncreaseAmount})")
+    print(f"Greatest Decrease in Profits: {minIncreaseMonth} (${minIncreaseAmount})")
 
     #export the results to a text file
